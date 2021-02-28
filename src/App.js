fa74b3d83f9dac29e782832a2fe9855694063599
M. Hafiz Bukhari <hafiz.bukhari@hotmail.com>
2021-02-28 12:22:25 +0700

    das

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState({
+    inputA:0,
+    inputB:0,
+    result:0
+  })
+
+  function method(param){
+    let {inputA, inputB} = number
+    if (param == "+") setNumber({...number, result:inputA + inputB})
+    else if (param == "-") setNumber({...number, result: inputA - inputB})
+    else if (param == "/") setNumber({...number, result: inputA / inputB})
+    else if (param == "*") setNumber({...number, result: inputA * inputB})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className = "container">
+      <div className = "card">
+        <div className = "header">
+          My Calculator
+        </div>
+        <div className = "body">
+          <div className = "inputWrapper">
+            <input type="number" placeholder = "inputA" onInput = {(e)=>setNumber({...number,  inputA: parseInt(e.target.value) })}/>
+            <input type="number" placeholder = "inputB" onInput = {(e)=>setNumber({...number, inputB: parseInt(e.target.value)})}/>
+            <input type="text" placeholder = "result" value = {number.result}/>
+          </div>
+          <div className = "inputWrapper">
+            <button onClick = {()=>method("+")}>+</button>
+            <button onClick = {()=>method("-")}>-</button>
+            <button onClick = {()=>method("/")}>/</button>
+            <button onClick = {()=>method("*")}>*</button>
+          </div>
+          
+
+        </div>
+        <div className = "footer">
+          
+        </div>
+      </div>
     </div>
+   </>
   );
 }
 
